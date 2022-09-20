816f05589ada6119ed09635908c1a4d27e8e0782
ZofiaZinkowska <zinkowskazosia20@gmail.com>
2022-09-20 21:21:55 +0200

    zmiany

--- aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/Model/RegisterEntry.cs
+++ aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/Model/RegisterEntry.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace aplikacja_webowa___praca_inzynierska.Model
 {
--- aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/Startup.cs
+++ aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/Startup.cs
@@ -1,13 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace aplikacja_webowa___praca_inzynierska
 {
@@ -24,6 +20,7 @@ namespace aplikacja_webowa___praca_inzynierska
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +47,7 @@ namespace aplikacja_webowa___praca_inzynierska
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
