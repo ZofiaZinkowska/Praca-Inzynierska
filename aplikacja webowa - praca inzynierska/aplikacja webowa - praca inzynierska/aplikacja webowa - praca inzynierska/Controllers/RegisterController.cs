f77a6af7f79ad9acb36f0a2fb3b5f5c7461b1328
ZofiaZinkowska <zinkowskazosia20@gmail.com>
2022-09-20 20:52:14 +0200

    Add controller RegisterEntry

---
+++ aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/Controllers/RegisterController.cs
@@ -0,0 +1,44 @@
+﻿using aplikacja_webowa___praca_inzynierska.Model;
+using LiteDB;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace aplikacja_webowa___praca_inzynierska.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private const string DbName = "Register.db";
+        private const string RegisterEntryCollectionName = "RegisterEntries";
+
+        [HttpPost]
+        public ActionResult<RegisterEntry> Save(RegisterEntry registerEntry)
+        {
+            //Open database connection
+            using (var db = new LiteDatabase(DbName))
+            {
+                //Get RegisterEntry collection
+                var collection = db.GetCollection<RegisterEntry>(RegisterEntryCollectionName);
+
+                //Insert new RegisterEntry
+                collection.Insert(registerEntry);
+
+                return registerEntry;
+            }
+        }
+
+        public ActionResult<RegisterEntry> Get(int id)
+        {
+            //Open database connection
+            using (var db = new LiteDatabase(DbName))
+            {
+                //Get RegisterEntry collection
+                var collection = db.GetCollection<RegisterEntry>(RegisterEntryCollectionName);
+
+                //Find item by id
+                var registerEntry = collection.FindOne(e => e.Id == id);
+
+                return registerEntry;
+            }
+        }
+    }
+}
---
+++ aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/Model/RegisterEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aplikacja_webowa___praca_inzynierska.Model
+{
+    public class RegisterEntry
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime AddDate { get; set; }
+    }
+}
