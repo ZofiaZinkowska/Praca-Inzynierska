59a5c9fc7fbe3b1a25afcede4563942b9382a20c
ZofiaZinkowska <zinkowskazosia20@gmail.com>
2022-09-21 19:32:54 +0200

    add update entry and not found

--- aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/Controllers/RegisterController.cs
+++ aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/Controllers/RegisterController.cs
@@ -5,13 +5,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace aplikacja_webowa___praca_inzynierska.Controllers
 {
+    [Route("Register")]
+
     public class RegisterController : Controller
     {
         private const string DbName = "Register.db";
         private const string RegisterEntryCollectionName = "RegisterEntries";
 
-        [HttpPost]
-        public ActionResult<RegisterEntry> Save(RegisterEntry registerEntry)
+        [HttpPost("Save")]
+        public ActionResult<RegisterEntry> Save([FromBody]RegisterEntry registerEntry)
         {
             //Open database connection
             using (var db = new LiteDatabase(DbName))
@@ -19,12 +21,14 @@ namespace aplikacja_webowa___praca_inzynierska.Controllers
                 //Get RegisterEntry collection
                 var collection = db.GetCollection<RegisterEntry>(RegisterEntryCollectionName);
 
-                //Insert new RegisterEntry
-                collection.Insert(registerEntry);
+                //Upsert RegisterEntry (insert or update existing)
+                collection.Upsert(registerEntry);
 
                 return registerEntry;
             }
         }
+
+        [HttpGet("Get")]
 
         public ActionResult<RegisterEntry> Get(int id)
         {
@@ -35,7 +39,9 @@ namespace aplikacja_webowa___praca_inzynierska.Controllers
                 var collection = db.GetCollection<RegisterEntry>(RegisterEntryCollectionName);
 
                 //Find item by id
-                var registerEntry = collection.FindOne(e => e.Id == id);
+                var registerEntry = collection.FindById(id);
+
+                if (registerEntry is null) return NotFound();
 
                 return registerEntry;
             }
