9eecdee0e4a7b6a8fd00c5be7bd5a7555c79d727
ZofiaZinkowska <zinkowskazosia20@gmail.com>
2022-10-03 20:14:54 +0200

    ListView.vue

--- aplikacja webowa - praca inzynierska/frontend/src/views/ListView.vue
+++ aplikacja webowa - praca inzynierska/frontend/src/views/ListView.vue
@@ -1,3 +1,17 @@
 <template>
-    roślinki
+    <b-container class ="shadow-sm rounded my-3 p-4 bg-body" >
+        <h4 class ="border-bottom pb-2">Moje roślinki</h4>
+        <b-table-simple>
+            <b-thead>
+                <b-tr>
+                    <b-th>Id</b-th>
+                    <b-th>Nazwa</b-th>
+                    <b-th>Data dodania</b-th>
+                </b-tr>
+            </b-thead>
+            <b-tbody>
+                
+            </b-tbody>
+        </b-table-simple>
+    </b-container>
 </template>
