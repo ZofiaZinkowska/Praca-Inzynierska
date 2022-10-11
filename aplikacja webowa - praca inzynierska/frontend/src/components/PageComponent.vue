ccc75f71e229535a0ff2b8afbed3549d2309b258
ZofiaZinkowska <zinkowskazosia20@gmail.com>
2022-10-11 21:08:45 +0200

    Add Alert.ts and PageComponent.vue

---
+++ aplikacja webowa - praca inzynierska/frontend/src/components/PageComponent.vue
@@ -0,0 +1,22 @@
+<template>
+        <b-alert class="my-3 shadow-sm" :variant="alert?.type" dismissible="true" :show="alert">{{alert?.text}}</b-alert>
+    <b-container class="shadow-sm rounded my-3 p-4 bg-body">
+        <h4 class="border-bottom pb-2">Ewidencja</h4>
+        <div class="d-flex justify-content-center mt-5 mb-3" v-if="isBusy">
+            <b-spinner></b-spinner>
+        </div>
+        <slot v-else/>
+    </b-container>
+</template>
+<script lang="ts">
+import { defineComponent, type PropType } from 'vue';
+import type {Alert} from './Alert';
+
+export default defineComponent({
+    props: {
+        alert: Object as PropType<Alert>,
+        title: String,
+        isBusy: Boolean
+    }
+});
+</script>
