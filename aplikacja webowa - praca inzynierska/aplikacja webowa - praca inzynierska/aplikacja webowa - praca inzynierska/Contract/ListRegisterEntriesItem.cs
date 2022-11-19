dbeef27735732f0857a7052b5c8bb0f9116fecc5
ZofiaZinkowska <zinkowskazosia20@gmail.com>
2022-11-19 23:39:31 +0100

    zmiany

--- aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/Contract/ListRegisterEntriesItem.cs
+++ aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/aplikacja webowa - praca inzynierska/Contract/ListRegisterEntriesItem.cs
@@ -7,6 +7,8 @@ namespace aplikacja_webowa___praca_inzynierska.Contract
         public int Id { get; set; }
         public string ScientificName { get; set; }
         public string ScientificNameAuthor { get; set; }
+
+        //TODO: format daty
         public DateTime AddDate { get; set; }
     }
 }
