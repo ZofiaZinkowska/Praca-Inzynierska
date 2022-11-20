dc8a1a2ea9eec7a07931a4cb02046aa0f4b5ec4d
ZofiaZinkowska <zinkowskazosia20@gmail.com>
2022-11-20 20:40:55 +0100

    SpinnerComponent.vue AddView.vue

---
+++ aplikacja webowa - praca inzynierska/frontend/src/components/SpinnerComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="root">
+        <slot></slot>
+        <div class="spinner d-flex justify-content-center align-items-center" v-if="isVisible" >
+            <b-spinner></b-spinner>
+        </div>
+    </div>
+</template>
+<style scoped>
+.root{
+    position: relative;
+}
+.spinner{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(255,255,255,0.7);
+}
+</style>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    props: {
+        isVisible: {type: Boolean, default: false}
+    }
+});
+</script>
--- aplikacja webowa - praca inzynierska/frontend/src/views/AddView.vue
+++ aplikacja webowa - praca inzynierska/frontend/src/views/AddView.vue
@@ -2,34 +2,41 @@
     <page-component title="Dodawanie nowego wpisu" :alert="alert">
         <div class="mt-4">
             <b-form-group label="Zeskanuj kod rośliny">
-               <b-form-input v-model="code" ref="codeInput" type="search" placeholder="Zeskanuj kod rośliny..." @search="find()"></b-form-input>
+                <b-form-input v-model="code" ref="codeInput" type="search" placeholder="Zeskanuj kod rośliny..."
+                    @search="find()"></b-form-input>
             </b-form-group>
-            <b-form-group label="Klasyfikacja roślin">
-                <taxonomy-selector-component ref="taxonomyInput" @item-selected="taxonomyItem=$event" placeholder="Wyszukaj pozycję w klasyfikacji..."></taxonomy-selector-component>
-            </b-form-group>
+            <spinner-component :is-visible="isFinding">
+                <b-form-group label="Klasyfikacja roślin">
+                    <taxonomy-selector-component ref="taxonomyInput" @item-selected="taxonomyItem = $event"
+                        placeholder="Wyszukaj pozycję w klasyfikacji..."></taxonomy-selector-component>
+                </b-form-group>
+            </spinner-component>
             <div class=" d-flex justify-content-end">
                 <span v-if="!!code && !!taxonomyItem">Kod zostanie powiązany z wybraną rośliną</span>
-                <b-button :disabled="isSaving" @click="$router.back()" variant="secondary" class="me-2">Anuluj</b-button>
-                <b-button :loading="isSaving" :disabled="isSaving || !isValid" @click="save()" variant="success">Zapisz</b-button>
+                <b-button :disabled="isSaving" @click="$router.back()" variant="secondary" class="me-2">Anuluj
+                </b-button>
+                <b-button :loading="isSaving" :disabled="isSaving || !isValid" @click="save()" variant="success">Zapisz
+                </b-button>
             </div>
         </div>
     </page-component>
 </template>
 <script lang="ts">
 import axios from 'axios';
-import type {SaveRegisterEntryRequest} from '../contract/SaveRegisterEntryRequest';
+import type { SaveRegisterEntryRequest } from '../contract/SaveRegisterEntryRequest';
 import { defineComponent } from 'vue';
 import PageComponent from '../components/PageComponent.vue';
 import type { Alert } from '@/components/Alert';
 import TaxonomySelectorComponent from '../components/TaxonomySelectorComponent.vue';
 import type { SearchTaxonomyItem } from '@/contract/SearchTaxonomyItem';
-import type {SaveTaxonomyCodeRequest} from '@/contract/SaveTaxonomyCodeRequest';
+import type { SaveTaxonomyCodeRequest } from '@/contract/SaveTaxonomyCodeRequest';
+import SpinnerComponent from '../components/SpinnerComponent.vue';
 
-interface Data {code?:string; taxonomyItem?: SearchTaxonomyItem; alert?: Alert; isSaving:boolean; }
+interface Data { isFinding: boolean; code?: string; taxonomyItem?: SearchTaxonomyItem; alert?: Alert; isSaving: boolean; }
 
 export default defineComponent({
     data(): Data {
-        return {code: undefined, taxonomyItem: undefined, alert: undefined, isSaving: false };
+        return { isFinding: false, code: undefined, taxonomyItem: undefined, alert: undefined, isSaving: false };
     },
     mounted() {
         const input = this.$refs.codeInput as HTMLInputElement;
@@ -37,36 +44,37 @@ export default defineComponent({
     },
     computed: {
         isValid() {
-            return !! this.taxonomyItem;
+            return !!this.taxonomyItem;
         }
     },
     methods: {
         async save() {
             try {
-                this.alert=undefined;
+                this.alert = undefined;
                 this.isSaving = true;
-                const request: SaveRegisterEntryRequest={taxonomyID: this.taxonomyItem!.taxonomyID};
+                const request: SaveRegisterEntryRequest = { taxonomyID: this.taxonomyItem!.taxonomyID };
                 await axios.put("https://localhost:5001/Register/Add", request);
-                if (!!this.code){
-                    const codeRequest: SaveTaxonomyCodeRequest={taxonomyID: this.taxonomyItem!.taxonomyID, code: this.code};
+                if (!!this.code) {
+                    const codeRequest: SaveTaxonomyCodeRequest = { taxonomyID: this.taxonomyItem!.taxonomyID, code: this.code };
                     await axios.put("https://localhost:5001/Taxonomy/AddCode", codeRequest);
                 }
                 this.$router.push({ name: "List" });
             }
             catch {
-                this.alert = {type:"danger", text:"Wystąpił błąd"};
+                this.alert = { type: "danger", text: "Wystąpił błąd" };
             }
             finally {
                 this.isSaving = false;
             }
         },
 
-        async find(){
+        async find() {
             if (!this.code)
                 return;
-            try{
+            try {
                 this.alert = undefined;
-                const response = await axios.get<SearchTaxonomyItem[]>("https://localhost:5001/Taxonomy/Find", {params:{code: this.code}});
+                this.isFinding = true;
+                const response = await axios.get<SearchTaxonomyItem[]>("https://localhost:5001/Taxonomy/Find", { params: { code: this.code } });
                 const items = response.data;
                 if (items.length === 0) {
                     return;
@@ -75,10 +83,13 @@ export default defineComponent({
                 input.selectItem(items[0]);
             }
             catch {
-                this.alert = {type:"danger", text:"Wystąpił błąd"};
+                this.alert = { type: "danger", text: "Wystąpił błąd" };
+            }
+            finally {
+                this.isFinding = false;
             }
         }
     },
-    components: { PageComponent, TaxonomySelectorComponent }
+    components: { PageComponent, TaxonomySelectorComponent, SpinnerComponent }
 });
 </script>
