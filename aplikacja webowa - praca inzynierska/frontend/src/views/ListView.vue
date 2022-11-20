5a847ec844408af3467a60651450d503ab780048
ZofiaZinkowska <zinkowskazosia20@gmail.com>
2022-11-20 20:08:48 +0100

    Praca inżynierska ListView.vue (data)

--- aplikacja webowa - praca inzynierska/frontend/src/views/ListView.vue
+++ aplikacja webowa - praca inzynierska/frontend/src/views/ListView.vue
@@ -2,25 +2,32 @@
     <page-component title="Wykaz roślin" :is-busy="isBusy" :alert="alert">
         <template #before-spinner>
             <div class="d-flex justify-content-end">
-            <b-input-group class="me-2">
-                <taxonomy-selector-component @item-selected="load($event?.taxonomyID)" placeholder="Wyszukaj w ewidencji..."></taxonomy-selector-component>
-            </b-input-group>
+                <b-input-group class="me-2">
+                    <taxonomy-selector-component @item-selected="load($event?.taxonomyID)"
+                        placeholder="Wyszukaj w ewidencji..."></taxonomy-selector-component>
+                </b-input-group>
             </div>
-        </template>        
+        </template>
         <b-table-simple>
             <b-thead>
                 <b-tr>
                     <b-th>Id</b-th>
-                    <b-th><sort-toggle-component sort-by="name" :value="sort" @toggled="onSort">Nazwa naukowa (Autor)</sort-toggle-component></b-th>
-                    <b-th><sort-toggle-component sort-by="date" :value="sort" @toggled="onSort">Data dodania</sort-toggle-component></b-th>
+                    <b-th>
+                        <sort-toggle-component sort-by="name" :value="sort" @toggled="onSort">Nazwa naukowa (Autor)
+                        </sort-toggle-component>
+                    </b-th>
+                    <b-th>
+                        <sort-toggle-component sort-by="date" :value="sort" @toggled="onSort">Data dodania
+                        </sort-toggle-component>
+                    </b-th>
                     <b-th></b-th>
                 </b-tr>
             </b-thead>
             <b-tbody>
                 <b-tr v-for="item in items">
-                    <b-td>{{item.id}}</b-td>
-                    <b-td>{{item.scientificName}} ({{item.scientificNameAuthor}})</b-td>
-                    <b-td>{{item.addDate}}</b-td>
+                    <b-td>{{ item.id }}</b-td>
+                    <b-td>{{ item.scientificName }} ({{ item.scientificNameAuthor }})</b-td>
+                    <b-td :title="formatDateTime(item.addDate)">{{ formatDate(item.addDate) }}</b-td>
                     <b-td class=" d-flex justify-content-end">
                         <b-button variant="danger" @click="remove(item)" class="btn-sm">Usuń</b-button>
                     </b-td>
@@ -40,50 +47,59 @@ import { remove } from '@vue/shared';
 import SortToggleComponent from '../components/SortToggleComponent.vue';
 import type { SortDescription } from '@/components/SortDescription';
 import TaxonomySelectorComponent from '../components/TaxonomySelectorComponent.vue';
+import moment from 'moment';
 
-interface Data { items: ListRegisterEntriesItem[]; alert?: Alert; isBusy:boolean;
-                sort?: SortDescription; }
+interface Data {
+    items: ListRegisterEntriesItem[]; alert?: Alert; isBusy: boolean;
+    sort?: SortDescription;
+}
 
 export default defineComponent({
     data(): Data {
         return { items: [], alert: undefined, isBusy: false, sort: undefined };
     },
-    methods:{
-        async load(keyword?: string){
-            try {
-            this.alert=undefined;
-            this.isBusy = true;
-            var response = await axios.get<ListRegisterEntriesItem[]>("https://localhost:5001/Register/List",{
-                params: {keyword, sortBy: this.sort?.by, sortDirection: this.sort?.direction}
-            });
-            this.items = response.data;
-        }
-        catch {
-            this.alert = {type:"danger", text:"Wystąpił błąd"};;
-        }
-        finally {
-            this.isBusy = false;
-        }
+    methods: {
+        formatDate(date: string) {
+            return moment(date).format('DD.MM.yyyy');
         },
-        async remove(item: ListRegisterEntriesItem){
-            try {
-            this.alert=undefined;
-            this.isBusy = true;
-            const itemIndex = this.items.indexOf(item);
-            await axios.delete(`https://localhost:5001/Register/Delete?id=${item.id}`);
-            this.items.splice(itemIndex, 1);
-        }
-        catch {
-            this.alert = {type:"danger", text:"Wystąpił błąd"};;
-        }
-        finally {
-            this.isBusy = false;
-        }
+        formatDateTime(dateTime: string){
+            return moment(dateTime).format('DD.MM.yyyy HH:mm:ss')
         },
-        async onSort(sortDescription: SortDescription|undefined){
-        this.sort = sortDescription;
-        await this.load();
-    },
+        async load(keyword?: string) {
+            try {
+                this.alert = undefined;
+                this.isBusy = true;
+                var response = await axios.get<ListRegisterEntriesItem[]>("https://localhost:5001/Register/List", {
+                    params: { keyword, sortBy: this.sort?.by, sortDirection: this.sort?.direction }
+                });
+                this.items = response.data;
+            }
+            catch {
+                this.alert = { type: "danger", text: "Wystąpił błąd" };;
+            }
+            finally {
+                this.isBusy = false;
+            }
+        },
+        async remove(item: ListRegisterEntriesItem) {
+            try {
+                this.alert = undefined;
+                this.isBusy = true;
+                const itemIndex = this.items.indexOf(item);
+                await axios.delete(`https://localhost:5001/Register/Delete?id=${item.id}`);
+                this.items.splice(itemIndex, 1);
+            }
+            catch {
+                this.alert = { type: "danger", text: "Wystąpił błąd" };;
+            }
+            finally {
+                this.isBusy = false;
+            }
+        },
+        async onSort(sortDescription: SortDescription | undefined) {
+            this.sort = sortDescription;
+            await this.load();
+        },
     },
     async mounted() {
         await this.load();
